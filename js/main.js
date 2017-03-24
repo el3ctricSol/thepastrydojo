a2734737c87a8a319cd2394ad64f4564890392ca
student <student@305-08.aicahlabs.edu>
2017-03-23 21:51:24 -0700

    good for final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// navigation
+
 var icon = document.getElementById("icon");
 var icon1 = document.getElementById("a");
 var icon2 = document.getElementById("b");
@@ -11,4 +13,25 @@ icon.addEventListener('click', function() {
     icon3.classList.toggle('b');
     nav.classList.toggle('show');
     blue.classList.toggle('slide');
+});
+
+
+
+// fade transition betweeen pages
+
+var speed = 'slow';
+
+$('html, body').hide();
+
+$(document).ready(function() {
+    $('html, body').fadeIn(speed, function() {
+        $('a[href], button[href]').click(function(event) {
+            var url = $(this).attr('href');
+            if (url.indexOf('#') == 0 || url.indexOf('javascript:') == 0) return;
+            event.preventDefault();
+            $('html, body').fadeOut(speed, function() {
+                window.location = url;
+            });
+        });
+    });
 });
